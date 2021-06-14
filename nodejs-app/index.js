a9a8750ff7822b5cc1b9b7f27c213efe459389b3
Yash Kapila <yash.kapila18@gmail.com>
2021-06-14 20:06:55 +0200

    initial commit

---
+++ nodejs-app/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 80;
+
+// App
+const app = express();
+
+app.post('/api/log', (req, res) => {
+  console.log('API LOGGING!!!');
+  res.send('Successfully API logged!!!');
+});
+
+app.post('/log', (req, res) => {
+  console.log('LOGGING!!!');
+  res.send('Successfully logged!!!');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const server = app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
